88ec465490424222773f87cd80b0cf64945933ad
Alexandra <alexandra15dem@gmail.com>
2022-10-17 12:19:38 +0300

    Second sort (Alexandra)

--- task.py
+++ task.py
@@ -1,9 +1,21 @@
-def insertionSort(array):
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i-1
-        while array[j] > key and j >= 0:
-            array[j+1] = array[j]
-            j -= 1
-        array[j+1] = key
-    return array
+def sortArray(self, nums: List[int]) -> List[int]:
+    i_lower_bound, upper_bound = min(nums), max(nums)
+    lower_bound = i_lower_bound
+    if i_lower_bound < 0:
+        lb = abs(i_lower_bound)
+        nums = [item + lb for item in nums]
+        lower_bound, upper_bound = min(nums), max(nums)
+
+    counter_nums = [0] * (upper_bound - lower_bound + 1)
+    for item in nums:
+        counter_nums[item - lower_bound] += 1
+    pos = 0
+    for idx, item in enumerate(counter_nums):
+        num = idx + lower_bound
+        for i in range(item):
+            nums[pos] = num
+            pos += 1
+    if i_lower_bound < 0:
+        lb = abs(i_lower_bound)
+        nums = [item - lb for item in nums]
+    return nums
