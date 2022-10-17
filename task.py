82f8d0e5abeb8f067bd7b5de7cfe4d783e2f436e
Alexandra Barieva <alex.genrietta@gmail.com>
2022-10-17 12:13:08 +0300

    add sort shell

--- task.py
+++ task.py
@@ -7,3 +7,19 @@ def insertionSort(array):
             j -= 1
         array[j+1] = key
     return array
+
+def shellSort(array):
+    n = len(array)
+    k = int(math.log2(n))
+    interval = 2**k -1
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+            array[j] = temp
+        k -= 1
+        interval = 2**k -1
+    return array
